de810c147f5f14bfccd29b7ce7c32d6c0e2069d5
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-13 09:27:46 +0100

    added alt attribute change

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ const actionButton = document.querySelector(".btn");
 actionButton.addEventListener("click", function () {
   if (actionButton.innerHTML.includes("Accendi")) {
     changeImg.src = "./img/yellow_Lamp.png";
+    changeImg.alt = "On Lamp";
     actionButton.innerHTML = "Spegni";
   } else if (actionButton.innerHTML.includes("Spegni")) {
     changeImg.src = "./img/white_Lamp.png";
+    changeImg.alt = "Off Lamp";
     actionButton.innerHTML = "Accendi";
   }
 });
